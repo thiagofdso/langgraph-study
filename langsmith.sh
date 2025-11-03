a3bb204e5117ef2bbd6934cef0aa72c4de715d58
Thiago Oliveira <thiagofdso.ufpa@gmail.com>
2025-11-03 10:18:05 -0300

    Atualizando .gitignore

--- langsmith.sh
+++ langsmith.sh
@@ -1,5 +1,5 @@
 service docker start
-langgraph dev --config langgraph.json --host 0.0.0.0
 cd nginx && docker-compose down && docker-compose rm && docker-compose up -d && cd ..
 echo "Acesse a URL:"
 echo "https://smith.langchain.com/studio/?baseUrl=https://langgraph.local"
+langgraph dev --config langgraph.json --host 0.0.0.0
